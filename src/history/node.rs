b17a2e408598baaa02a94b744c07f3486a10295a
Joao Koritar <jakoritarleite@gmail.com>
2022-08-29 09:09:40 -0300

    tests: confirm that nodes are being created and linked correctly

--- src/history/node.rs
+++ src/history/node.rs
@@ -21,3 +21,32 @@ impl Node {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::{Node, RefCell};
+
+    #[test]
+    fn should_create_root_node() {
+        let node = Node::new(None, None);
+
+        assert_eq!(node.prev, None);
+        assert_eq!(node.next, RefCell::new(None));
+    }
+
+    #[test]
+    fn should_link_node_and_root() {
+        let root = Node::new(None, None);
+
+        assert_eq!(root.prev, None);
+        assert_eq!(root.next, RefCell::new(None));
+
+        let node = Node::new(Some(root.id.clone()), None);
+
+        assert_eq!(node.prev, Some(root.id.clone()));
+        assert_eq!(node.next, RefCell::new(None));
+
+        root.next.replace(Some(node.id.clone()));
+    
+        assert_eq!(root.next, RefCell::new(Some(node.id.clone())));
+    }
+}
